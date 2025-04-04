e3a0d87385de443518916fa7dba2b6429c466283
Harshit Shivhare <harshit.owo@gmail.com>
2025-04-05 02:04:51 +0530

    feat: added inventory component

---
+++ app/components/inventoryUl.tsx
@@ -0,0 +1,25 @@
+import styles from "../cssmodules/inventory.module.css";
+import Image from "next/image";
+export default function InventoryUl(
+  items: { stickerUrl: string; stickerName: string; stickter: string }[],
+) {
+  return (
+    <ul className={styles.itemUl}>
+      {items.map((item, idx) => (
+        <li className={styles.itemLi} key={idx}>
+          <h3>{item.stickerName}</h3>
+          <div className={styles.itemLiDiv}>
+            <Image
+              src={item.stickerUrl}
+              alt={item.stickerName}
+              width={200}
+              height={200}
+              className={styles.imageEle}
+            />
+            <p>{item.stickter}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
