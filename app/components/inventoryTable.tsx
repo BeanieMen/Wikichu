313ef673b8401548344905c453a6163e32981ad0
Harshit Shivhare <harshit.owo@gmail.com>
2025-04-05 10:46:25 +0530

    fix: fixed ui artifacts

--- app/components/inventoryTable.tsx
+++ app/components/inventoryTable.tsx
@@ -19,20 +19,16 @@ export default function InventoryTable({ items }: InventoryTableProps) {
       <h2 className="text-2xl font-semibold text-center mb-6">Inventory</h2>
 
       {/* Table Structure */}
-      <div className="overflow-x-auto text-black">
+      <div className="overflow-x-wrap text-black">
         <table className="min-w-full table-auto border-collapse border border-gray-300">
           <thead>
             <tr className="bg-gray-100">
-              <th className="px-4 py-2 border border-gray-300 text-center">
+              <th className="p-1 border border-gray-300 text-center">
                 Sticker
               </th>
-              <th className="px-4 py-2 border border-gray-300 text-center">
-                Name
-              </th>
-              <th className="px-4 py-2 border border-gray-300 text-center">
-                Rarity
-              </th>
-              <th className="px-4 py-2 border border-gray-300 text-center">
+              <th className="p-1 border border-gray-300 text-center">Name</th>
+              <th className="p-1 border border-gray-300 text-center">Rarity</th>
+              <th className="p-1 border border-gray-300 text-center">
                 Description
               </th>
             </tr>
@@ -41,25 +37,25 @@ export default function InventoryTable({ items }: InventoryTableProps) {
             {items.map((item, index) => (
               <tr key={index} className="odd:bg-white even:bg-gray-50">
                 {/* Image as a Table Column */}
-                <td className="px-4 py-2 border border-gray-300 text-center">
+                <td className="p-1 border border-gray-300 text-center">
                   <Image
                     alt={item.name}
                     src={"/globe.svg"}
-                    width={50}
-                    height={50}
+                    width={25}
+                    height={25}
                     className="rounded-md m-auto"
                   />
                 </td>
                 {/* Name as a Table Column */}
-                <td className="px-4 py-2 border border-gray-300 text-center">
+                <td className="p-1 border border-gray-300 text-center">
                   {item.name}
                 </td>
                 {/* Rarity as a Table Column */}
-                <td className="px-4 py-2 border border-gray-300 text-center">
+                <td className="p-1 border border-gray-300 text-center">
                   {rarityMap[item.rarity - 1]}
                 </td>
                 {/* Description as a Table Column */}
-                <td className="px-4 py-2 border border-gray-300 text-center">
+                <td className="p-1 border border-gray-300 text-center">
                   {item.stickerDesc}
                 </td>
               </tr>
